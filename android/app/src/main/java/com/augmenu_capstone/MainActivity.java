c9fd2ce3501c1941b30dc3602222ad1620b7e10e
Lindsay Maher <lindsaymah3r@gmail.com>
2018-03-16 17:11:35 -0400

    here we go

---
+++ android/app/src/main/java/com/augmenu_capstone/MainActivity.java
@@ -0,0 +1,15 @@
+package com.augmenu_capstone;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "augmenu_capstone";
+    }
+}
